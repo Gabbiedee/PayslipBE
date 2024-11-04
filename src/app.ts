ff31916f484136aa626924e2ca3e891f06eb1411
Dolapo <akingbojudolly@gmail.com>
2024-11-04 17:23:39 +0100

    update middleware

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,33 @@
-import express from "express"
-import errorHandler from "./middleware/errorHandler"
-import router from "./routes/authRoute"
+import express from "express";
+import errorHandler from "./middleware/errorHandler";
+import router from "./routes/authRoute";
 import userAuth from "./middleware/authmiddleware";
 import employeeRouter from "./routes/employeeRoute";
 import homeRouter from "./routes/homeRoute";
-import cors from 'cors';
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors());
-
-
+app.use(cors()); // Enable CORS for all routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Public route that doesn't require authentication
+app.get("/dashboard", (req, res) => {
+    res.send({
+        message: "You are at home",
+    });
+});
 
-app.use("/", homeRouter )
-app.use("/employee", userAuth, employeeRouter)
-app.use("/auth", router)
+// Apply userAuth middleware only to routes that need authentication
+app.use("/employee", userAuth, employeeRouter);
+app.use("/auth", router);
 
+// If you have other routes for home, you can include them here
+// For example:
+// app.use("/", homeRouter); // Unprotected routes could be added
 
-
-
-app.get("/dashboard" ,(req, res)=>{
-res.send({
-    message: "You are at home"
-})
-})
-app.use(errorHandler)
-
+// Error handling middleware should be the last middleware
+app.use(errorHandler);
 
 export default app;
