441f83af467c45cf06c4b41b866d8e014c82b5af
Dolapo <akingbojudolly@gmail.com>
2024-11-14 07:39:24 +0100

    Update Cors with vercel

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-    origin: 'https://payslip-smoky.vercel.app/', 
+    origin: 'https://payslip-smoky.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true, 
   };
